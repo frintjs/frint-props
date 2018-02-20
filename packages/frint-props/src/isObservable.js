1291db3eb8e18c7507c446ffdc79ebc5e2171187
leandrooriente <leandroriente@gmail.com>
2018-02-20 13:38:00 +0100

    Add reference to the original package

--- packages/frint-props/src/isObservable.js
+++ packages/frint-props/src/isObservable.js
@@ -1,3 +1,4 @@
+// https://github.com/sindresorhus/is-observable
 import symbolObservable from 'symbol-observable';
 
 const isObservable = value => Boolean(value && value[symbolObservable] && value === value[symbolObservable]());
