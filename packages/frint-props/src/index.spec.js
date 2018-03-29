a89f872e98ab61d831d49a802f81443282d813f1
Slava Zhuravski <szhuravski@travix.com>
2018-03-29 10:18:56 +0200

    Make tests concise

--- packages/frint-props/src/index.spec.js
+++ packages/frint-props/src/index.spec.js
@@ -6,39 +6,13 @@ describe('frint-props :: index', function () {
     expect(typeof lib).toBe('object');
   });
 
-  test('compose is a function', function () {
-    expect(typeof lib.compose).toBe('function');
-  });
-
-  test('map is a function', function () {
-    expect(typeof lib.map).toBe('function');
-  });
-
-  test('pipe is a function', function () {
-    expect(typeof lib.pipe).toBe('function');
-  });
-
-  test('shouldUpdate is a function', function () {
-    expect(typeof lib.shouldUpdate).toBe('function');
-  });
-
-  test('withDefaults is a function', function () {
-    expect(typeof lib.withDefaults).toBe('function');
-  });
-
-  test('withHandlers is a function', function () {
-    expect(typeof lib.withHandlers).toBe('function');
-  });
-
-  test('withObservable is a function', function () {
-    expect(typeof lib.withObservable).toBe('function');
-  });
-
-  test('withState is a function', function () {
-    expect(typeof lib.withState).toBe('function');
-  });
-
-  test('withStore is a function', function () {
-    expect(typeof lib.withStore).toBe('function');
+  [
+    'compose', 'map', 'pipe', 'shouldUpdate',
+    'withDefaults', 'withHandlers', 'withObservable',
+    'withState', 'withStore',
+  ].forEach((name) => {
+    test(`${name} is a function`, function () {
+      expect(typeof lib[name]).toBe('function');
+    });
   });
 });
