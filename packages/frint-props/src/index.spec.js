e66c65ffa12f25e50fd89f7b0c19a62bde93e8e5
Viacheslav Zhuravski <viacheslaff@users.noreply.github.com>
2018-03-29 11:44:27 +0200

    Export withHandlers() from frint-props (#34)
    * Export withHandlers() from frint-props
    
    * Make tests concise

--- packages/frint-props/src/index.spec.js
+++ packages/frint-props/src/index.spec.js
@@ -5,4 +5,14 @@ describe('frint-props :: index', function () {
   test('is an object', function () {
     expect(typeof lib).toBe('object');
   });
+
+  [
+    'compose', 'map', 'pipe', 'shouldUpdate',
+    'withDefaults', 'withHandlers', 'withObservable',
+    'withState', 'withStore',
+  ].forEach((name) => {
+    test(`${name} is a function`, function () {
+      expect(typeof lib[name]).toBe('function');
+    });
+  });
 });
